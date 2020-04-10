c36fcd27e5d367eb6540c2c4a171b67a3c2c1ef7
Connor Davis <connor@wisticdigital.com>
2020-04-10 11:51:24 -0700

    Completed our build process configuration

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,5 +1,21 @@
 import '../styles/styles.css';
 
+// React Related Code
+// import React from 'react';
+// import ReaconDOM from 'react-dom';
+
+// function MyAmazingComponent()
+// {
+//     return (
+//         <div>
+//             <h1>This is My Amazing React Component</h1>
+//             <p>React is great</p>
+//         </div>
+//     );
+// }
+
+// ReactDOM.render(<MyAmazingCompononent />, document.querySelector("#my-react-example"));
+
 if (module.hot)
 {
     module.hot.accept()
